17c78e9a496a5d0554707ee24ad0353aa33aa2f2
almamun0101 <almamunkhan0101@gmail.com>
2025-04-09 23:20:26 +0600

    ain

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <>
+      <div className="bg-pri border-b-amber-50 broder-white">
+        <nav className="flex items-center justify-between text-thi container">
+            <div className="logo">
+            <h2 className="text-2xl font-[Oswald] font-bold tracking-wider">SaulDesign</h2>
+            </div>
+            <div className="navbar">
+                <ul className="flex gap-12 py-10">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Services</a></li>
+                </ul>
+            </div>
+        </nav>
+      </div>
+    </>
+  );
+};
